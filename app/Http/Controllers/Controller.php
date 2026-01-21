5385a90e8f6911fc15c312e415106a06e32c769d
Developer <dev@vericrown.local>
2026-01-21 16:17:51 -0600

    Fix Laravel bootstrap issues: create storage/cache directories, fix Controller class, remove artisan post-install scripts, set APP_KEY

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,5 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+namespace App\Http\Controllers;
 
-$request = app(Request::class);
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, ValidatesRequests;
+}
